862a761faeb00890b00d36dfb71a673fc54569fc
Shahid Malik <54556036+shahidmalikmail@users.noreply.github.com>
2020-07-30 05:05:21 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,2 +1,11 @@
-Var = "This is Adeeba malik and Aayat Malik"
-print
+adeeba = "Adeeba is Good Girl and Going to School"
+
+print(len(adeeba))
+#print(adeeba[::])
+print(adeeba.upper())
+
+print(adeeba.find("and"))
+print(adeeba.replace("is", "beta"))
+print(adeeba.endswith("School"))
+
+
